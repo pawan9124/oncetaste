36540fb70f022719a949dfbaf68c64ce4cda1afa
Pawan Saket <pawan@vujadelabs.com>
2019-07-21 18:02:16 +0530

    Pouring files

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const webpack = require("webpack");
 const webpackDevMiddleware = require("webpack-dev-middleware");
 
 const app = express();
+const path = require("path");
+const port = process.env.PORT || 8080;
 const config = require("./webpack.config.js");
 const compiler = webpack(config);
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/dist"));
 
 //Tell express to use the webpack-dev-middleware and use the webpack.config.js
 //configuration file as a base.
@@ -15,7 +21,12 @@ app.use(
   })
 );
 
+// send the user to index html page inspite of the url
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "/public/index.html"));
+});
+
 //Serve the files on port 3000.
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Example app listening on port 3000:\n");
 });
